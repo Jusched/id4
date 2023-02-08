5c76c83a653eff8733d99f7eeacb0a6c6e374aea
Jusched <juans-betancurt@unilibre.edu.co>
2023-02-08 16:40:47 -0500

    finished readme, added logs. finished.

---
+++ README.md
@@ -0,0 +1,26 @@
+# 4ID Login/Logout with FastAPI
+
+## Introduction
+
+This is a FastAPI application that provides endpoints for user signup and logout.
+
+## Requirements
+
+- Python 3.6+
+
+## Usage
+
+1. Install the required packages by running `pip install -r requirements.txt`
+2. Run the application by executing `uvicorn main:app --reload`
+3. Signup a user by making a `POST` request to `/signup` with a JSON payload containing `email` and `password`. The `email` must be unique and the `password` must be longer than 8 characters.
+4. Logout a user by making a `POST` request to `/logout` with a JSON payload containing the `email` of the user.
+
+## Endpoints
+
+- `/signup`: This endpoint is used to register a new user. It accepts a JSON payload containing the user's `email` and `password`. Upon successful signup, the registered user's information will be returned in a JSON object.
+- `/logout`: This endpoint is used to log out a user. It accepts a JSON payload containing the user's `email`. A successful logout will result in a message confirming that the user has been logged out.
+- `/clear`: This endpoint is used to clear the database.
+
+## Note
+
+It's important to note that before the password is stored in the database, it's encrypted using the `encrypt_password()` utility for added security.
--- main.py
+++ main.py
@@ -7,9 +7,21 @@ from models.users import User, UserIn
 from config.database import Base, Session, engine
 from utils.utils import encrypt_password
 
+# Logger
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+handler = logging.FileHandler('api.log')
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
+
 
 app = FastAPI()
-app.title = "Login/logout API. 4ID"
+app.title = "4ID Login/logout API"
 
 Base.metadata.create_all(bind=engine)
 
@@ -21,8 +33,11 @@ def signup(user: UserIn):
     Receives an email (unique) and a password.
     """
 
+    logging.info("Starting Signup endpoint.")
     if len(user.password) < 8:
+        logger.error("Exception: Password must be longer than 8 characters.")
         raise HTTPException(400, "Password must be longer than 8 characters.")
+    
     
     db = Session()
 
@@ -33,6 +48,7 @@ def signup(user: UserIn):
 
     db.add(new_user)
     db.commit()
+    logging.info("Signup endpoint completed.")
     return user
 
 
@@ -40,17 +56,22 @@ def signup(user: UserIn):
 def logout(email: str):
     """Logs out the user based on provided email."""
 
+    logging.debug(f"Received request to logout user with email: {email}")
     db = Session()
     user = db.query(User).filter(User.email == email).first()
+
     if not user:
+        logging.debug(f"Exception: User with email: {email} not found.")
         raise HTTPException(400, "Email not found.")
     
     if not user.session_id:
-        raise HTTPException(400, "Email is already logged out.")
+        logging.debug(f"User with email: {email} already logged out.")
+        raise HTTPException(400, "Exception: Email is already logged out.")
     
     user.session_id = None
     db.add(user)
     db.commit()
+    logging.info(f"User with email: {email} successfully logged out.")
     return Response(content="Logged out successfully.")
 
 
@@ -59,6 +80,11 @@ def clear_db():
     """Deletes everything in the database
     and builds it up again."""
 
+    logging.debug("Received request to clear database.")
+
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
+    logging.info("Database cleared and recreated successfully.")
     return 
+
+
